17ae5a82b57f0814c29fff11103c8ab2fa5de71f
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-26 23:18:46 +0530

    second commit

--- pass.py
+++ pass.py
@@ -1,3 +1,14 @@
-password = input('Enter your password here: ')
+import re
 
+print('Password Strength Checker')
+password = input('Enter your password here: ')
+lng_error =len(password) < 8
+
+password_good = not (lng_error)
+if password_good:
+    print('strong pass')
+
+else: 
+    if lng_error:
+        print('error')
 print(password)
