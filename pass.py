92980dc41569767eee1c7a30d2bf4b93e6620697
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-27 09:00:11 +0530

    third commit

--- pass.py
+++ pass.py
@@ -3,12 +3,7 @@ import re
 print('Password Strength Checker')
 password = input('Enter your password here: ')
 lng_error =len(password) < 8
-
-password_good = not (lng_error)
-if password_good:
-    print('strong pass')
-
-else: 
-    if lng_error:
-        print('error')
+upper_error = re.search(r"[A-Z]", password is None)
+lower_error = re.search(r"[a-z]", password is None)
+special_error = re.search(r"[!]")
 print(password)
