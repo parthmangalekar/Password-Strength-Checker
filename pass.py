83864f9edf2695a67eb3501f2b14fd50b4120523
parthmangalekar <parthmangalekar2004@gmail.com>
2026-02-27 09:07:10 +0530

    fourth commit

--- pass.py
+++ pass.py
@@ -3,7 +3,8 @@ import re
 print('Password Strength Checker')
 password = input('Enter your password here: ')
 lng_error =len(password) < 8
+digit_error = re.search(r"/d", password is None)
 upper_error = re.search(r"[A-Z]", password is None)
 lower_error = re.search(r"[a-z]", password is None)
-special_error = re.search(r"[!]")
+special_error = re.search(r"[!#\$%&'\(\)\*\+,\-\./:;<=>\?@\[\\\]\^_`\{\|\}~]", password) is None
 print(password)
